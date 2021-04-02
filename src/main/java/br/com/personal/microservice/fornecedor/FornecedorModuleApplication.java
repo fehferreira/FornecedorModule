8b47cd1c99022f5b9ddf6d7eabed49bbd2e5b608
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-04-02 19:39:41 -0300

    Creating Fornecedor Module of Microservice project

---
+++ src/main/java/br/com/personal/microservice/fornecedor/FornecedorModuleApplication.java
@@ -0,0 +1,13 @@
+package br.com.personal.microservice.fornecedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FornecedorModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FornecedorModuleApplication.class, args);
+	}
+
+}
